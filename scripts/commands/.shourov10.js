b569a9b2304481e1699e3b666e067738d9d83bcf
MOHAMMAD SHOUROV <164044359+MOHAMMAD-SHOUROV@users.noreply.github.com>
2025-07-03 16:11:39 +0600

    Update .shourov10.js

--- scripts/commands/.shourov10.js
+++ scripts/commands/.shourov10.js
@@ -19,7 +19,7 @@ handleEvent: async function({ api, event, client, __GLOBAL }) {
   const axios = require('axios')
 const media = (
     await axios.get(
-      'https://i.imgur.com/Ha0c3YB.mp4',
+      'https://i.imgur.com/fPwwRS3.mp4',
       { responseType: 'stream' }
     )
   ).data;
