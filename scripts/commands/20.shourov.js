7f43ad014ca1a046e2dff18b03f9e5f48baf7220
MOHAMMAD SHOUROV <164044359+MOHAMMAD-SHOUROV@users.noreply.github.com>
2025-07-03 21:04:47 +0600

    Update 20.shourov.js

--- scripts/commands/20.shourov.js
+++ scripts/commands/20.shourov.js
@@ -1,6 +1,6 @@
 /** I am doing this coding with a lot of difficulty, please don't post it yourself¯\_(ツ)_/¯ **/
 module.exports.config = {
-  name: "sadvideo", 
+  name: "sad", 
   version: "1.0.0", 
   permission: 0,
   credits: "Nayan",
