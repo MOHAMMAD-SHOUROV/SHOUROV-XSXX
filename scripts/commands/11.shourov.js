86fa585b670c23cb8e825e966d5a37e994ba6d7d
MOHAMMAD SHOUROV <164044359+MOHAMMAD-SHOUROV@users.noreply.github.com>
2025-07-03 20:54:56 +0600

    Update 11.shourov.js

--- scripts/commands/11.shourov.js
+++ scripts/commands/11.shourov.js
@@ -3,7 +3,7 @@ const fs = require("fs-extra");
 const path = require("path");
 
 module.exports.config = {
-  name: "ss",
+  name: "shourov",
   version: "1.0.0",
   permission: 0,
   credits: "farhan",
