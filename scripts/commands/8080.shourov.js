6a7e4258f86fca42e818ea3c4b2b215c9b66f258
MOHAMMAD SHOUROV <164044359+MOHAMMAD-SHOUROV@users.noreply.github.com>
2025-07-03 23:07:39 +0600

    Update 8080.shourov.js

--- scripts/commands/8080.shourov.js
+++ scripts/commands/8080.shourov.js
@@ -1,38 +1,47 @@
-const fs = require("fs");
+const axios = require("axios");
+
 module.exports = {
-  config:{
-	name: "fahim6",
-        version: "1.0.1",
-        prefix: false,
-	permssion: 0,
-	credits: "nayan", 
-	description: "Fun",
-	category: "no prefix",
-	usages: "😒",
-        cooldowns: 5, 
-},
+  config: {
+    name: "fahim6",
+    version: "1.0.1",
+    prefix: false,
+    permission: 0,
+    credits: "nayan",
+    description: "Fun video reply command",
+    category: "no prefix",
+    usages: "😒",
+    cooldowns: 5,
+  },
 
-handleEvent: async function({ api, event, client, __GLOBAL }) {
-	var { threadID, messageID } = event;
-  const content = event.body ? event.body : '';
-  const body = content.toLowerCase();
-  const axios = require('axios')
-const media = (
-    await axios.get(
-      'https://i.imgur.com/6EaYYaU.mp4',
-      { responseType: 'stream' }
-    )
-  ).data;
+  handleEvent: async function({ api, event }) {
+    const { threadID, messageID, body } = event;
+    if (!body) return;
 
-	if (body.indexOf("bura beti")==0 || body.indexOf("😵‍💫")==0) {
-		var msg = {
-				body: "𝐊𝐢𝐧𝐠_𝐒𝐡𝐨𝐮𝐫𝐨𝐯 ",
-				attachment: media
-			}
-			api.sendMessage( msg, threadID, messageID);
-    api.setMessageReaction("😓", event.messageID, (err) => {}, true)
-		}
-	},
-	start: function({ nayan }) {
+    const lowerBody = body.toLowerCase();
+
+    if (lowerBody.startsWith("bura beti") || lowerBody.startsWith("😵‍💫")) {
+      try {
+        const videoUrl = "https://i.imgur.com/6EaYYaU.mp4";
+
+        const response = await axios.get(videoUrl, {
+          responseType: "stream"
+        });
+
+        const msg = {
+          body: "𝐊𝐢𝐧𝐠_𝐒𝐡𝐨𝐮𝐫𝐨𝐯",
+          attachment: response.data
+        };
+
+        api.sendMessage(msg, threadID, messageID);
+        api.setMessageReaction("😓", messageID, (err) => {}, true);
+
+      } catch (error) {
+        console.error("❌ ভিডিও আনতে সমস্যা হয়েছে:", error.message);
+      }
+    }
+  },
+
+  start: function() {
+    // Optional startup logic
   }
-  }
+};
