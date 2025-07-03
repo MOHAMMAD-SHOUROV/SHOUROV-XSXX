2331f487e3dacb439669db308779de1c0d470fd0
MOHAMMAD SHOUROV <164044359+MOHAMMAD-SHOUROV@users.noreply.github.com>
2025-07-03 21:11:18 +0600

    Update 6.fuck.shourov.js

--- scripts/commands/6.fuck.shourov.js
+++ scripts/commands/6.fuck.shourov.js
@@ -1,22 +1,25 @@
 module.exports = {
- config: {
-	 name: "fuck you",
-	 version: "1.0",
-	 author: "AceGun",
-	 countDown: 5,
-	 role: 0,
-	 shortDescription: "no prefix",
-	 longDescription: "no prefix",
-	 category: "no prefix",
- },
+  config: {
+    name: "fuck you",
+    version: "1.0",
+    author: "AceGun",
+    countDown: 5,
+    role: 0,
+    shortDescription: "Responds to rude message",
+    longDescription: "Replies with a GIF when someone says 'fuck'",
+    category: "no prefix"
+  },
 
- onStart: async function(){}, 
- onChat: async function({ event, message, getLang }) {
- if (event.body && event.body.toLowerCase() === "fuck") {
- return message.reply({
- body: "Fuck you too🖕",
- attachment: await global.utils.getStreamFromURL("https://i.imgur.com/9bNeakd.gif")
- });
- }
- }
-}
+  onStart: async function () {},
+
+  onChat: async function({ event, message }) {
+    const text = event.body?.toLowerCase();
+
+    if (text === "fuck") {
+      return message.reply({
+        body: "Fuck you too 🖕",
+        attachment: await global.utils.getStreamFromURL("https://i.imgur.com/9bNeakd.gif")
+      });
+    }
+  }
+};
