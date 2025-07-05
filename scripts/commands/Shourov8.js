e90788158f61285713a868101b9113c670dd1a59
MOHAMMAD SHOUROV <164044359+MOHAMMAD-SHOUROV@users.noreply.github.com>
2025-07-05 12:18:25 +0600

    Update Shourov8.js

--- scripts/commands/Shourov8.js
+++ scripts/commands/Shourov8.js
@@ -19,7 +19,7 @@ handleEvent: async function({ api, event, client, __GLOBAL }) {
   const axios = require('axios')
 const media = (
     await axios.get(
-      'https://i.imgur.com/HIcgCoV.mp4',
+      'https://files.catbox.moe/1bx2l9.mp4',
       { responseType: 'stream' }
     )
   ).data;
