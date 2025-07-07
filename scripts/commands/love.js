71f30a3641557221599a227263082134c4ebeccf
MOHAMMAD SHOUROV <164044359+MOHAMMAD-SHOUROV@users.noreply.github.com>
2025-07-07 13:39:12 +0600

    Update love.js

--- scripts/commands/love.js
+++ scripts/commands/love.js
@@ -15,12 +15,14 @@ module.exports.config = {
   cooldowns: 5,
 };
 
+const loveDir = path.join(__dirname, "cache", "love_template");
+const bgURL = "https://i.imgur.com/5uUfYUj.jpeg";
+const bgPath = path.join(loveDir, "template.png");
+
 module.exports.onLoad = async () => {
-  const dir = __dirname + "/cache/Picsart_25-07-07_12-54-20-382.png";
-  if (!fs.existsSync(dir)) fs.mkdirSync(dir, { recursive: true });
-  const bgPath = path.join(dir, "template.png");
+  if (!fs.existsSync(loveDir)) fs.mkdirSync(loveDir, { recursive: true });
   if (!fs.existsSync(bgPath)) {
-    await global.utils.downloadFile("https://i.imgur.com/5uUfYUj.jpeg", bgPath); // Custom love bg
+    await global.utils.downloadFile(bgURL, bgPath);
   }
 };
 
@@ -34,24 +36,25 @@ module.exports.run = async function ({ event, api }) {
   const mention = Object.keys(event.mentions);
   const one = event.senderID;
   const two = mention[0];
-  if (!two)
-    return api.sendMessage("⚠️ একজনকে ট্যাগ করুন এই লাভ ইমেজ তৈরির জন্য!", event.threadID, event.messageID);
 
-  const loveDir = path.join(__dirname, "cache", "Picsart_25-07-07_12-54-20-382.png");
-  const bgPath = path.join(loveDir, "template.png");
+  if (!two) {
+    return api.sendMessage("⚠️ একজনকে ট্যাগ করুন এই লাভ ইমেজ তৈরির জন্য!", event.threadID, event.messageID);
+  }
+
   const avtPath1 = path.join(loveDir, `avt_${one}.png`);
   const avtPath2 = path.join(loveDir, `avt_${two}.png`);
   const finalPath = path.join(loveDir, `love_${one}_${two}.png`);
 
-  const getAvt = async (id, filePath) => {
-    const res = await axios.get(`https://graph.facebook.com/${id}/picture?width=512&height=512&access_token=6628568379|c1e620fa708a1d5696fb991c1bde5662`, {
-      responseType: "arraybuffer",
-    });
+  const getAvatar = async (id, filePath) => {
+    const res = await axios.get(
+      `https://graph.facebook.com/${id}/picture?width=512&height=512&access_token=6628568379|c1e620fa708a1d5696fb991c1bde5662`,
+      { responseType: "arraybuffer" }
+    );
     fs.writeFileSync(filePath, Buffer.from(res.data, "utf-8"));
   };
 
-  await getAvt(one, avtPath1);
-  await getAvt(two, avtPath2);
+  await getAvatar(one, avtPath1);
+  await getAvatar(two, avtPath2);
 
   const bg = await jimp.read(bgPath);
   const img1 = await circleImage(avtPath1);
@@ -70,12 +73,7 @@ module.exports.run = async function ({ event, api }) {
     {
       body: `❤️ ${event.mentions[two].replace("@", "")}, তোমার জন্য একটা লাভ মেমোরি ✨`,
       attachment: fs.createReadStream(finalPath),
-      mentions: [
-        {
-          tag: event.mentions[two],
-          id: two,
-        },
-      ],
+      mentions: [{ tag: event.mentions[two], id: two }],
     },
     event.threadID,
     () => {
