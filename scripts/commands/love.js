f4e881cbaf68556bd43990792d0a59c4ad1f9e2e
MOHAMMAD SHOUROV <164044359+MOHAMMAD-SHOUROV@users.noreply.github.com>
2025-07-08 15:27:46 +0600

    Update love.js

--- scripts/commands/love.js
+++ scripts/commands/love.js
@@ -1,97 +1,88 @@
 const fs = require("fs-extra");
-const axios = require("axios");
 const path = require("path");
+const axios = require("axios");
 const jimp = require("jimp");
 
 module.exports.config = {
-  name: "shourovlove",
+  name: "love",
   version: "1.0.0",
   hasPermission: 0,
-  credits: "King_Shourov",
-  description: "Love-style DP generator",
+  credits: "RAJA ViP 5X - Fixed by King_Shourov",
+  description: "Love-themed image generator",
   commandCategory: "media",
-  usages: "",
+  usages: "@mention",
   cooldowns: 5,
-  prefix: true
 };
 
 module.exports.onLoad = async () => {
-  const canvasPath = path.join(__dirname, "cache", "canvas");
-  const imgPath = path.join(canvasPath, "shourovlove.png");
+  const dir = path.join(__dirname, "cache");
+  const bgPath = path.join(dir, "Shourov.png");
 
-  if (!fs.existsSync(canvasPath)) fs.mkdirSync(canvasPath, { recursive: true });
-
-  if (!fs.existsSync(imgPath)) {
-    const res = await axios.get("https://i.imgur.com/r81XGpZ.jpeg", { responseType: "arraybuffer" });
-    fs.writeFileSync(imgPath, Buffer.from(res.data, "binary"));
+  if (!fs.existsSync(dir)) fs.mkdirSync(dir, { recursive: true });
+  if (!fs.existsSync(bgPath)) {
+    await global.utils.downloadFile("https://i.imgur.com/TOj5hnO.jpeg", bgPath);
   }
 };
 
-async function circle(imagePath) {
-  const img = await jimp.read(imagePath);
-  img.circle();
-  return await img.getBufferAsync("image/png");
-}
-
 async function makeImage({ one, two }) {
-  const canvasPath = path.join(__dirname, "cache", "canvas");
-  const bgPath = path.join(canvasPath, "shourovlove.png");
-  const pathImg = path.join(canvasPath, `love_${one}_${two}.png`);
-  const avtPath1 = path.join(canvasPath, `avt_${one}.png`);
-  const avtPath2 = path.join(canvasPath, `avt_${two}.png`);
+  const __root = path.join(__dirname, "cache");
+  const bgPath = path.join(__root, "Shourov.png");
 
-  // Download avatars
-  const getAvatar = async (uid, filePath) => {
-    const res = await axios.get(
-      `https://graph.facebook.com/${uid}/picture?width=512&height=512&access_token=6628568379|c1e620fa708a1d5696fb991c1bde5662`,
-      { responseType: "arraybuffer" }
-    );
-    fs.writeFileSync(filePath, Buffer.from(res.data, "binary"));
+  const avatarOnePath = path.join(__root, `avt_${one}.png`);
+  const avatarTwoPath = path.join(__root, `avt_${two}.png`);
+  const outPath = path.join(__root, `love_${one}_${two}.png`);
+
+  const getAvatar = async (id, filePath) => {
+    const url = `https://graph.facebook.com/${id}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`;
+    const response = await axios.get(url, { responseType: "arraybuffer" });
+    fs.writeFileSync(filePath, Buffer.from(response.data, "utf-8"));
   };
 
-  await getAvatar(one, avtPath1);
-  await getAvatar(two, avtPath2);
+  await getAvatar(one, avatarOnePath);
+  await getAvatar(two, avatarTwoPath);
 
-  // Read and process
+  const circleCrop = async (imgPath) => {
+    const img = await jimp.read(imgPath);
+    img.circle();
+    return img;
+  };
+
   const bg = await jimp.read(bgPath);
-  const img1 = await jimp.read(await circle(avtPath1));
-  const img2 = await jimp.read(await circle(avtPath2));
+  const circleOne = await circleCrop(avatarOnePath);
+  const circleTwo = await circleCrop(avatarTwoPath);
 
-  img1.resize(230, 230);
-  img2.resize(232, 232);
+  // Positioning and resizing
+  bg.composite(circleOne.resize(230, 230), 93, 122);
+  bg.composite(circleTwo.resize(232, 232), 513, 124);
 
-  bg.composite(img1, 93, 122);
-  bg.composite(img2, 513, 124);
+  await bg.writeAsync(outPath);
 
-  await bg.writeAsync(pathImg);
+  fs.unlinkSync(avatarOnePath);
+  fs.unlinkSync(avatarTwoPath);
 
-  fs.unlinkSync(avtPath1);
-  fs.unlinkSync(avtPath2);
-
-  return pathImg;
+  return outPath;
 }
 
 module.exports.run = async function ({ event, api }) {
-  const mention = Object.keys(event.mentions);
-  const one = event.senderID;
-  const two = mention[0];
+  const { threadID, messageID, senderID, mentions } = event;
+  const mentionIDs = Object.keys(mentions);
 
-  if (!two) return api.sendMessage("⚠️ দয়া করে কাউকে ট্যাগ করুন!", event.threadID, event.messageID);
+  if (mentionIDs.length === 0) {
+    return api.sendMessage("💌 ভালোবাসার মানুষটিকে ট্যাগ করুন!", threadID, messageID);
+  }
+
+  const one = senderID;
+  const two = mentionIDs[0];
 
   try {
-    const imgPath = await makeImage({ one, two });
-    return api.sendMessage(
-      {
-        body: "︵💚🌸︵\n\n-𝗙𝗮𝘃𝗼𝗿𝗶𝘁𝗲 𝗶𝗻 𝘁𝗵𝗶𝘀 𝗰𝗶𝘁𝘆 𝗶𝘀 𝘄𝗿𝗶𝘁𝗶𝗻𝗴 𝗻𝗼𝘃𝗲𝗹𝘀 𝗯𝘆 𝗽𝗮𝘀𝘀𝗶𝗼𝗻 𝗻𝗼𝘁 𝗹𝗼𝘃𝗲 -!!🙂💔🐰\n\n_এই শহরে আবেগ দ্বারা উপন্যাস লেখা হয় ভালোবাসা না-!!🖤🌸🐰𝐊𝐢𝐧𝐠_𝐒𝐡𝐨𝐮𝐫𝐨𝐯",
-        attachment: fs.createReadStream(imgPath),
-        mentions: [{ tag: event.mentions[two], id: two }]
-      },
-      event.threadID,
-      () => fs.unlinkSync(imgPath),
-      event.messageID
-    );
+    const imagePath = await makeImage({ one, two });
+    const msg = {
+      body: "︵💚🌸︵\n\n-𝗙𝗮𝘃𝗼𝗿𝗶𝘁𝗲 𝗶𝗻 𝘁𝗵𝗶𝘀 𝗰𝗶𝘁𝘆 𝗶𝘀 𝘄𝗿𝗶𝘁𝗶𝗻𝗴 𝗻𝗼𝘃𝗲𝗹𝘀 𝗯𝘆 𝗽𝗮𝘀𝘀𝗶𝗼𝗻 𝗻𝗼𝘁 𝗹𝗼𝘃𝗲 -!!🙂💔🐰\n\n_এই শহরে আবেগ দ্বারা উপন্যাস লেখা হয় ভালোবাসা না-!!🖤🌸🐰 𝐊𝐢𝐧𝐠_𝐒𝐡𝐨𝐮𝐫𝐨𝐯",
+      attachment: fs.createReadStream(imagePath),
+    };
+    api.sendMessage(msg, threadID, () => fs.unlinkSync(imagePath), messageID);
   } catch (err) {
-    console.error(err);
-    api.sendMessage("❌ ছবি তৈরিতে ত্রুটি ঘটেছে!", event.threadID, event.messageID);
+    console.error("Error in love command:", err);
+    api.sendMessage("❌ কিছু ভুল হয়েছে ছবি তৈরিতে। পরে আবার চেষ্টা করুন!", threadID, messageID);
   }
 };
