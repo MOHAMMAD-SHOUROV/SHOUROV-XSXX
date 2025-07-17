a6582bd02d9e112c568a4651a4afa42a377aa473
MOHAMMAD SHOUROV <164044359+MOHAMMAD-SHOUROV@users.noreply.github.com>
2025-07-18 00:39:01 +0600

    Update join.js

--- scripts/events/join.js
+++ scripts/events/join.js
@@ -106,11 +106,11 @@ https://www.facebook.com/www.xsxx.com365
         let avtAnime = (await axios.get(encodeURI(
           `https://graph.facebook.com/${event.logMessageData.addedParticipants[o].userFbId}/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`), { responseType: "arraybuffer" })).data;
         var ok = [
-          'https://imgur.com/a/07L7UdX',
-          'https://imgur.com/a/07L7UdX',
-          'https://imgur.com/a/07L7UdX',
-          'https://imgur.com/a/07L7UdX',
-          'https://imgur.com/a/07L7UdX'
+          'https://i.imgur.com/M7HEAMA.jpeg',
+          'https://i.imgur.com/V5L9dPi.jpeg',
+          'https://i.imgur.com/OYzHKNE.jpeg',
+          'https://i.imgur.com/UucSRWJ.jpeg',
+          'https://i.imgur.com/dDSh0wc.jpeg'
         ]
         let background = await new Promise((resolve, reject) => {
           request.get(
